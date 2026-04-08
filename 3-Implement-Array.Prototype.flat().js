e475c05fee6ccb15583a392e573f8c9a644ff89b
Prateek Rai <prateekrai341036@gmail.com>
2026-04-08 20:08:11 +0530

    Implemented the flat prototype

--- 3-Implement-Array.Prototype.flat().js
+++ 3-Implement-Array.Prototype.flat().js
@@ -4,3 +4,21 @@
  * @param { number } depth
  * @returns { Array }
  */
+\
+function flat(arr, depth = 1) {
+  let result = [];
+
+  for (let i=0; i<arr.length; i++){
+    if (!(i in arr)) continue;
+    if(Array.isArray(arr[i]) && depth > 0){
+      const outcome = flat(arr[i], depth - 1)
+      result = result.concat(outcome)
+    }else{
+      result.push(arr[i]);
+    }
+  } 
+  return result;
+}
+
+console.log(flat([1, [2], [3, [4]]], 1))
+
