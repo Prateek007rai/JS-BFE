fb2d22b37f5bd40698c317a31285efdf5387a3af
Prateek Rai <prateekrai341036@gmail.com>
2026-04-11 22:22:47 +0530

    made polyfill for map

--- Basics/2.Polyfill-map.js
+++ Basics/2.Polyfill-map.js
@@ -1,1 +1,15 @@
 // Polyfill - Map function
+
+Array.prototype.myMap = function(cb){
+    res = []
+
+    for (let i=0; i<this.length; i++){
+        res.push(cb(this[i], i, this))
+    }
+
+    return res
+}
+
+const arr = [1,2,3,4,5,6]
+
+console.log(arr.myMap((n)=> n*2))
