f02ff20a5a405b4fb55921b617ef5a36bb830f2b
Prateek Rai <prateekrai341036@gmail.com>
2026-04-15 21:57:25 +0530

    Solved rem duplicate (alllow max twice)

--- test.js
+++ test.js
@@ -150,3 +150,22 @@ function flatten (arr){
 
 console.log(flatten([1,2,[3,4,[5,6]]]))
 
+
+// Print max twice and remove duplicates
+function rem_dup(arr){
+   const counter = {}
+   const res = []
+
+   for (let num of arr){
+    counter[num] = (counter[num] || 0) + 1
+
+    if(counter[num] <= 2){
+        res.push(num)
+    }
+   }
+
+   console.log(counter)
+   return res
+}
+console.log(rem_dup([1,1,1,2,2,2,3,3,3,3,4,4]))
+
