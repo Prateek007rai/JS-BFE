dad0186862a534effa325fe22bdde5fcd9100746
Prateek Rai <prateekrai341036@gmail.com>
2026-04-23 16:05:31 +0530

    solved the problem

--- Interview Questions/4. print first Max freq occured char.js
+++ Interview Questions/4. print first Max freq occured char.js
@@ -1,2 +1,21 @@
 // i/p: ['a','b','c','d','a','b','c','e','b','a','c','f']
 // o/p: 'b'
+
+
+function count_freq(arr){
+    const counter = {}
+    let max_count = 0
+    let ele = ''
+    
+    for (let ch of arr){
+        counter[ch] = (counter[ch] || 0) + 1
+        
+        if(counter[ch] > max_count){
+            max_count = counter[ch]
+            ele = ch
+        }
+    }
+    return ele
+}
+
+console.log(count_freq(['a','b','c','d','a','b','c','e','b','a','c','f']))
