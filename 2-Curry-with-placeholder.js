181093a8a8cba7b748b520d1663242bd8f7728bc
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-19 23:38:55 +0530

    Update 2-Curry-with-placeholder.js

--- 2-Curry-with-placeholder.js
+++ 2-Curry-with-placeholder.js
@@ -59,4 +59,6 @@ const join = (a, b, c) => `${a}_${b}_${c}`;
 const curriedJoin = curry(join);
 const _ = curry.placeholder;
 
-
+console.log(curriedJoin(1, 2, 3));          // '1_2_3'
+console.log(curriedJoin(_, 2)(1, 3));       // '1_2_3'
+console.log(curriedJoin(_, _, _)(1)(_, 3)(2)); // '1_2_3'
