a81740075bfda09732773aa11b387ac33595323d
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-24 23:27:53 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
+//debounce function
+function debounce (fn, delay) {
+    let timer;
 
+    return function (...args){
+        clearTimeout(timer);
+
+        timer = setTimeout(()=>{
+            fn.apply(this, args)
+        }, delay)
+    }
+}
 
 //call debounce
 const callOn = debounce(()=> {console.log("See, it will print after delay")}, 1000)
