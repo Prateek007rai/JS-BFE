11f96c09b223c8790cc67b68f95207b10a5d9ea8
Prateek Rai <Prateekrai341036@gmail.com>
2026-07-04 23:01:04 +0530

    [practiced]date  function

--- test.js
+++ test.js
@@ -672,6 +672,6 @@ function debounce (fn, delay) {
 
 // add date in date
 const date = "2026-04-14"
-const newDate = new Date(date);
+// const newDate = new Date(date);
 
 newDate.setDate(newDate.getDate() + 1)
